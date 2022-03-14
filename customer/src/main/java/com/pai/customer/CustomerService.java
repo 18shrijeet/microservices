05d513e9e43fde7eb3c5b8c30b867291db6e8709
Shrijeet <shrijeetpai1998@gmail.com>
2022-03-14 22:21:54 +0530

    eureka server discovery

--- customer/src/main/java/com/pai/customer/CustomerService.java
+++ customer/src/main/java/com/pai/customer/CustomerService.java
@@ -18,7 +18,7 @@ public class CustomerService {
         // todo: check if valid email
         // todo: check if email is not taken
         FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                                "http://FRAUD/api/v1/fraud-check/{customerId}",
                                 FraudCheckResponse.class,customer.getId());
 
         assert fraudCheckResponse != null;
