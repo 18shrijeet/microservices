be4d3319b04139c26dc6e832f265cf7f84b1a4b4
Shrijeet <shrijeetpai1998@gmail.com>
2022-03-20 11:42:39 +0530

    refactoring microservices to use kubernetes

--- notification/src/main/java/com/pai/notification/NotificationApplication.java
+++ notification/src/main/java/com/pai/notification/NotificationApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         scanBasePackages = {
@@ -11,6 +13,9 @@ import org.springframework.context.annotation.Bean;
                 "com.pai.amqp"
         }
 )
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
